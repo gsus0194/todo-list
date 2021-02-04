4eb4d8c31f407795b2a69595814c58c13a056eb4
gsus0194 <j.villegascambero@gmail.com>
2021-02-04 17:32:10 -0600

    Added PrivateRoute component for user validation

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
 import React, { useEffect, useState } from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 import Navbar from "./components/Navbar";
+import { useStateValue } from "./context/StateProvider";
 import Account from "./pages/Account";
 import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
@@ -9,31 +15,51 @@ import { auth } from "./utils/firebase";
 
 const App = () => {
   const [firebaseUser, setFirebaseUser] = useState(false);
+  // eslint-disable-next-line no-empty-pattern
+  const [{}, dispatch] = useStateValue();
 
   useEffect(() => {
-    auth.onAuthStateChanged((user) => {
-      // console.log(user);
-      user ? setFirebaseUser(user) : setFirebaseUser(null);
-    });
+    const getUser = () => {
+      auth.onAuthStateChanged((user) => {
+        // console.log(user);
+        // console.log("userStorage>>>", localStorage.getItem("user"));
+        user ? setFirebaseUser(user) : setFirebaseUser(null);
+      });
+    };
+    getUser();
   }, []);
+
+  useEffect(() => {
+    if (localStorage.getItem("user")) {
+      dispatch({
+        type: "USER_SUCCESS",
+        payload: JSON.parse(localStorage.getItem("user")),
+      });
+    }
+  }, [dispatch]);
+
+  const PrivateRoute = ({ component, path, ...rest }) => {
+    if (localStorage.getItem("user")) {
+      const userStorage = JSON.parse(localStorage.getItem("user"));
+      if (userStorage.uid === firebaseUser.uid) {
+        return <Route component={component} path={path} {...rest} />;
+      } else {
+        return <Redirect to="login" {...rest} />;
+      }
+    } else {
+      return <Redirect to="login" {...rest} />;
+    }
+  };
 
   return (
     <Router>
-      <Navbar firebaseUser={firebaseUser}>
+      <Navbar>
         {firebaseUser !== false ? (
           <Switch>
-            <Route path="/login">
-              <Login />
-            </Route>
-            <Route path="/reset">
-              <Reset />
-            </Route>
-            <Route path="/account">
-              <Account user={firebaseUser} />
-            </Route>
-            <Route path="/dashboard">
-              <Dashboard user={firebaseUser} />
-            </Route>
+            <PrivateRoute component={Account} path="/account" />
+            <PrivateRoute component={Dashboard} path="/dashboard" />
+            <Route component={Login} path="/login" />
+            <Route component={Reset} path="/reset" />
             <Route path="/">Home</Route>
           </Switch>
         ) : (
